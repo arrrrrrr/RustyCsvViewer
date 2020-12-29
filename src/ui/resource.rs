58fd7eb2af016370fb9b93ca5f631e6fe009462f
Michael Z <i.am.the.ar@gmail.com>
2020-12-29 21:55:01 +1100

    created static string resource file for ui app

--- src/ui/resource.rs
+++ src/ui/resource.rs
@@ -1,3 +1,7 @@
 // This is the resource file where any static app values are stored
 pub const APP_TITLE: &str = "Rusty CSV Viewer";
+pub const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const APP_ABOUT: &str = "Copyright 2020";
 
+pub const APP_OPEN_FILE_DLG: &str = "Open a CSV file";
+pub const APP_OPEN_FILE_DLG_FILTER: &str = "CSV(*.csv)|Text(*.txt)|All Files(*.*)";
