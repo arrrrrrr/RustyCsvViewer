337ffa52fa81f16b022eb1b72ea0c7c2c8c3c83e
Michael Z <i.am.the.ar@gmail.com>
2020-12-30 03:52:35 +1100

    created 2 helpers for getting the current working directory in utils

---
+++ src/utils/mod.rs
@@ -0,0 +1,16 @@
+pub mod geometry;
+
+use std::env;
+use std::path::PathBuf;
+
+pub fn get_cwd() -> PathBuf {
+    let cwd = env::current_dir().unwrap_or(PathBuf::from("."));
+    let user_dir = env::var_os("USERPROFILE")
+        .expect("FATAL ERROR. USERPROFILE environment key missing");
+
+    cwd.canonicalize().unwrap_or(PathBuf::from(user_dir))
+}
+
+pub fn get_cwd_as_str() -> String {
+    get_cwd().to_string_lossy().to_string()
+}
